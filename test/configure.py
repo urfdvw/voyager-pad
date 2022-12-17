eaecc8f53f8e993c4b58dc6e1fbf778b3984d863
术生何 <urfdvw@gmail.com>
2022-12-17 18:49:02 -0500

    defined macro pad interface

---
+++ test/configure.py
@@ -0,0 +1,35 @@
+configure = {
+    -1: { # without modifier
+        2: [], # upper row
+        3: [],
+        4: [],
+        5: [],
+        6: [], # nob
+        7: [], # clock
+        8: [], # counterclock
+    },
+    0: { # with modifier
+        2: [], # upper row
+        3: [],
+        4: [],
+        5: [],
+        6: [], # nob
+        7: [], # clock
+        8: [], # counterclock
+    },
+    1: { # with modifier
+        2: [], # upper row
+        3: [],
+        4: [],
+        5: [],
+        6: [], # nob
+        7: [], # clock
+        8: [], # counterclock
+    },
+}
+
+"""
+available configure keys
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN', 'EIGHT', 'NINE', 'ZERO', 'ENTER', 'RETURN', 'ESCAPE', 'BACKSPACE', 'TAB', 'SPACEBAR', 'SPACE', 'MINUS', 'EQUALS', 'LEFT_BRACKET', 'RIGHT_BRACKET', 'BACKSLASH', 'POUND', 'SEMICOLON', 'QUOTE', 'GRAVE_ACCENT', 'COMMA', 'PERIOD', 'FORWARD_SLASH', 'CAPS_LOCK', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'PRINT_SCREEN', 'SCROLL_LOCK', 'PAUSE', 'INSERT', 'HOME', 'PAGE_UP', 'DELETE', 'END', 'PAGE_DOWN', 'RIGHT_ARROW', 'LEFT_ARROW', 'DOWN_ARROW', 'UP_ARROW', 'KEYPAD_NUMLOCK', 'KEYPAD_FORWARD_SLASH', 'KEYPAD_ASTERISK', 'KEYPAD_MINUS', 'KEYPAD_PLUS', 'KEYPAD_ENTER', 'KEYPAD_ONE', 'KEYPAD_TWO', 'KEYPAD_THREE', 'KEYPAD_FOUR', 'KEYPAD_FIVE', 'KEYPAD_SIX', 'KEYPAD_SEVEN', 'KEYPAD_EIGHT', 'KEYPAD_NINE', 'KEYPAD_ZERO', 'KEYPAD_PERIOD', 'KEYPAD_BACKSLASH', 'APPLICATION', 'POWER', 'KEYPAD_EQUALS', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'F21', 'F22', 'F23', 'F24', 'LEFT_CONTROL', 'CONTROL', 'LEFT_SHIFT', 'SHIFT', 'LEFT_ALT', 'ALT', 'OPTION', 'LEFT_GUI', 'GUI', 'WINDOWS', 'COMMAND', 'RIGHT_CONTROL', 'RIGHT_SHIFT', 'RIGHT_ALT', 'RIGHT_GUI', 
+'_RECORD', '_FAST_FORWARD', '_REWIND', '_SCAN_NEXT_TRACK', '_SCAN_PREVIOUS_TRACK', '_STOP', '_EJECT', '_PLAY_PAUSE', '_MUTE', '_VOLUME_DECREMENT', '_VOLUME_INCREMENT', '_BRIGHTNESS_DECREMENT', '_BRIGHTNESS_INCREMENT'
+"""
--- test/test_macropad_class.py
+++ test/test_macropad_class.py
@@ -1,5 +1,4 @@
 # cpy native
-import board
 import rotaryio
 import keypad
 
@@ -25,23 +24,8 @@ class EventQueue:
     def __bool__(self):
         return bool(self.data)
         
-class MacroPad:
-    def __init__(
-        self,
-        key_pins=(
-            board.D10, # lower row
-            board.D6,
-            board.D3, # upper row
-            board.D2,
-            board.D1,
-            board.D0,
-            board.D9, # nob
-        ),
-        encoder_pins=(
-            board.D8, # A
-            board.D7, # B
-        )
-    ):
+class MacroKeyPad:
+    def __init__(self, key_pins, encoder_pins):
         # hardware
         self.encoder=rotaryio.IncrementalEncoder(*encoder_pins)
         self.keys = keypad.Keys(
@@ -49,6 +33,7 @@ class MacroPad:
             pull=True,
             value_when_pressed=False
         )
+        self.n_keys = len(key_pins)
         # queue
         self._events = EventQueue()
         
@@ -56,7 +41,7 @@ class MacroPad:
     def events(self):
         position = self.encoder.position
         if position != 0:
-            key_number = 7 if position > 0 else 8
+            key_number = self.n_keys if position > 0 else self.n_keys + 1
             position = abs(position)
             for i in range(position):
                 self._events.append(
@@ -76,9 +61,64 @@ class MacroPad:
         while event := self.keys.events.get():
             self._events.append(event)
         return self._events
-    
 
-pad = MacroPad()
-while True:
-    if event := pad.events.get():
-        print(event)
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+
+Keycode_names = {Keycode.__dict__[k]: k for k in Keycode.__dict__ if k[0].isupper()}
+ConsumerControlCode_names = {ConsumerControlCode.__dict__[k]: k for k in ConsumerControlCode.__dict__ if k[0].isupper()}
+
+from configure import configure
+       
+class MacroPad:
+    def __init__(
+        self,
+        macrokeypad,
+        configure,
+        hid,
+    ):
+        self.macrokeypad = macrokeypad
+        self.configure = configure
+        
+        self.layer = 0
+        self.n_layer_key_press = 0
+        self.n_key_press = 0
+        
+        
+        self.kbd = Keyboard(hid.devices)
+        self.consumer_control = ConsumerControl(hid.devices)
+        
+    def __call__(self):
+        pass
+
+import board
+import usb_hid
+
+macrokeypad = MacroKeyPad(
+    key_pins=(
+        board.D10, # lower row
+        board.D6,
+        board.D3, # upper row
+        board.D2,
+        board.D1,
+        board.D0,
+        board.D9, # nob
+    ),
+    encoder_pins=(
+        board.D8, # A
+        board.D7, # B
+    )
+)
+
+macropad = MacroPad(
+    macrokeypad=macrokeypad,
+    configure=configure,
+    hid=usb_hid,
+)
+
+# while True:
+#     if event := macrokeypad.events.get():
+#         print(event)
